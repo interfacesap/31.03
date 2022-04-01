3ceb788d3b2d7f4bffe66f284df1fb910338c763
ErikaMessias <majmrs@gmail.com>
2022-04-01 11:20:35 -0300

    first commit

---
+++ Frontend-InterfaceSap/pages/cod_sucata/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="cont">
+    <headers />
+    <div class="page">
+      <div class="content-block">
+        <div class="titulo">
+          <h1>Codigo Sucata</h1>
+          <hr />
+        </div>
+        <table style="width:100%">
+          <tr>
+            <td>Emil</td>
+            <td>Tobias</td>
+            <td>Linus</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+    <footers />
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+@import '@/pages/cod_sucata/cod_sucata.scss';
+</style>
---
+++ Frontend-InterfaceSap/pages/historico/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="cont">
+    <headers />
+        <div class="page">
+          <div class="content-block">
+            <div class="titulo">
+              <h1>Historico</h1>
+              <hr>
+            </div>
+            <div class="pages">
+              
+            </div>
+          </div>
+        </div>
+    <footers />
+  </div>
+</template>
+
+<script>
+  
+</script>
+
+<style lang="scss" scoped>
+  @import '@/pages/historico/historico.scss'
+</style>
